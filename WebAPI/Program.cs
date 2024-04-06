78f6ccd474ef319176a9e9a96a2bae79185c7083
Taras Yavdoshniak <tarasyavdoshniak@gmail.com>
2024-04-06 12:01:13 +0300

    Add FreeCorsPolicy.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => options.AddPolicy(name: "FreeCorsPolicy", cfg =>
+{
+    cfg.AllowAnyHeader();
+    cfg.AllowAnyMethod();
+    cfg.WithOrigins("*");
+}));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -9,6 +15,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("FreeCorsPolicy");
 app.UseHttpsRedirection();
 
 app.MapGet("/api/test", () => new { Succeded = true });
