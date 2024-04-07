c882aa04d5fedcccd38ce0d3e22b01046f8f68ec
Ky4k <6965090@stud.nau.edu.ua>
2024-04-07 09:51:55 +0300

    Added main entities: 1) Order 2) User 3) Product 4) Enum with types of user

---
+++ DAL/Entities/UserType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.DAL.Entiries
+{
+    public enum UserType
+    {
+        Administrator,
+        Manager,
+        Consultant,
+        RegisteredUser,
+        UnregisteredUser
+    }
+}
