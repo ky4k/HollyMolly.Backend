9411b226821067b68d5b983e6174bd67aa74daef
Ky4k <6965090@stud.nau.edu.ua>
2024-05-01 01:37:52 +0300

    Added validation for support

---
+++ BLL/Validators/SupportDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using HM.BLL.Models.Supports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HM.BLL.Validators
+{
+    public class SupportDtoValidator: AbstractValidator<SupportDto>
+    {
+        public SupportDtoValidator()
+        {
+            RuleFor(x => x.Description)
+                .NotNull().WithMessage("Description cannot be null")
+                .NotEmpty().WithMessage("Description cannot be empty")
+                .Length(4, 500).WithMessage("Description must be between 4 and 500 characters")
+                .Matches("^[a-zA-Z0-9а-яА-ЯіІїЇєЄ !#$%&\"/?.,-_();:']+$")
+                .WithMessage("Description contains invalid characters");
+        }
+    }
+}
