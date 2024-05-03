bf473cc0497ec49f8ef080077191e505f964d004
Ky4k <6965090@stud.nau.edu.ua>
2024-05-03 12:04:56 +0300

    Added EmailService method for Support

--- BLL/Services/EmailService.cs
+++ BLL/Services/EmailService.cs
@@ -2,15 +2,27 @@
 using HM.BLL.Models.Common;
 using HM.BLL.Models.NewsSubscriptions;
 using HM.BLL.Models.Orders;
+using HM.BLL.Models.Supports;
 using HM.BLL.Models.Users;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 
 namespace HM.BLL.Services;
 
-public class EmailService(
-    IEmailSender emailSender
-    ) : IEmailService
+public class EmailService: IEmailService
 {
+    private readonly IEmailSender emailSender;
+    private readonly string supportEmail;
+
+    public EmailService(
+        IEmailSender emailSender,
+        IConfiguration configuration
+    )
+    {
+        this.emailSender = emailSender;
+        this.supportEmail = configuration["Support:Email"] ?? "default-support-email@example.com";
+    }
+
     private const string RegistrationSubject = "Реєстрація успішна";
     private const string RegistrationTemplate = "<p>Ласкаво просимо до Holly-Molly!</p><br /><p>Вітаємо, Ви успішно зареєструвалися в HollyMolly. Для підтвердження вашої електронної пошти перейдіть за посиланням: <a title=\"https://holly-molly.vercel.app/?userId={{userId}}&token={{token}}\" href=\"https://holly-molly.vercel.app/?userId={{userId}}&token={{token}}\">https://holly-molly.vercel.app/?userId={{userId}}&token={{token}}</a></p><p>___<br />З найкращими побажаннями, HollyMolly</p>";
     private const string ForgetPasswordSubject = "Відновити пароль";
@@ -25,6 +37,25 @@ public class EmailService(
     private const string OrderStatusUpdatedTemplate = "<p>Статус вашого замовлення з ідентифікатором №{{orderId}} було змінено на {{newStatus}}. Для відслідковування статусу замовлень перейдіть за посиланням та скористайтесь розділом \"Мої замовлення\"<br /><a title=\"https://holly-molly.vercel.app/\" href=\"https://holly-molly.vercel.app/\">https://holly-molly.vercel.app/</a></p></p><p>___<br />З найкращими побажаннями, HollyMolly</p>";
     private const string NewsTemplate = "{{news}}<p>____<br />Відписатися від новин можна за посиланням: <a title=\"https://holly-molly.vercel.app/?token={{token}}\" href=\"https://holly-molly.vercel.app/?token={{token}}\">https://holly-molly.vercel.app/?token={{token}}</a></p><p>___<br />З найкращими побажаннями, HollyMolly</p>";
 
+    public async Task<OperationResult> SendSupportEmailAsync(SupportDto supportRequest, CancellationToken cancellationToken)
+    {
+        // Формуємо тему та зміст листа
+        string subject = $"Новий запит підтримки від {supportRequest.Name}";
+        string emailBody = $@"
+                <h2>Новий запит підтримки</h2>
+                <p><strong>Ім'я:</strong> {supportRequest.Name}</p>
+                <p><strong>Email:</strong> {supportRequest.Email}</p>
+                <p><strong>Тема:</strong> {supportRequest.Topic}</p>
+                <p><strong>Опис:</strong> {supportRequest.Description}</p>
+            ";
+
+        UserMailInfo userMailInfo = new()
+        {
+            Email = supportEmail
+        };
+
+        return await emailSender.SendEmailAsync(userMailInfo, subject, emailBody, cancellationToken);
+    }
     public async Task<OperationResult> SendRegistrationResultEmailAsync(string email,
         ConfirmationEmailDto confirmationEmail, CancellationToken cancellationToken)
     {
