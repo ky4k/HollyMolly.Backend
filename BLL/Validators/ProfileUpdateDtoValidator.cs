a99f4fc473b14fc4fb0d8c81da2c20b3e64c8d31
Ky4k <6965090@stud.nau.edu.ua>
2024-07-24 17:40:28 +0300

    Updated controller NewPost
    Turn off validation CustomerCreateDtoValidatior, OrderCreateDtoValidator, ProfileUpdateDtoValidator for controller testing

--- BLL/Validators/ProfileUpdateDtoValidator.cs
+++ BLL/Validators/ProfileUpdateDtoValidator.cs
@@ -6,24 +6,24 @@ namespace HM.BLL.Validators;
 
 public class ProfileUpdateDtoValidator : AbstractValidator<ProfileUpdateDto>
 {
-    public ProfileUpdateDtoValidator(INewPostService newPostService)
-    {
-        RuleFor(pud => pud.FirstName)
-            .ApplyNameValidationRules();
-        RuleFor(pud => pud.LastName)
-            .ApplyNameValidationRules();
-        RuleFor(pud => pud.PhoneNumber)
-            .ApplyPhoneNumberValidationRules();
-        RuleFor(pud => pud.DateOfBirth)
-            .Must(db => db == null || db.Value < DateOnly.FromDateTime(DateTime.UtcNow))
-                .WithMessage("Date of birth must be in the past");
-        RuleFor(pud => pud.City)
-            .MustAsync(async (city, cancellation) => string.IsNullOrEmpty(city)
-                || await newPostService.CheckIfCityIsValidAsync(city, cancellation))
-                .WithMessage("The city name must be in the exact format provided by the New Post.");
-        RuleFor(pud => pud.DeliveryAddress)
-            .MustAsync(async (customer, address, cancellation) => address == null
-                || customer.City != null && await newPostService.CheckIfAddressIsValidAsync(customer.City, address, cancellation))
-                .WithMessage("There is no New Post office at the specified address in the city.");
-    }
+    //public ProfileUpdateDtoValidator(INewPostService newPostService)
+    //{
+    //    RuleFor(pud => pud.FirstName)
+    //        .ApplyNameValidationRules();
+    //    RuleFor(pud => pud.LastName)
+    //        .ApplyNameValidationRules();
+    //    RuleFor(pud => pud.PhoneNumber)
+    //        .ApplyPhoneNumberValidationRules();
+    //    RuleFor(pud => pud.DateOfBirth)
+    //        .Must(db => db == null || db.Value < DateOnly.FromDateTime(DateTime.UtcNow))
+    //            .WithMessage("Date of birth must be in the past");
+    //    RuleFor(pud => pud.City)
+    //        .MustAsync(async (city, cancellation) => string.IsNullOrEmpty(city)
+    //            || await newPostService.CheckIfCityIsValidAsync(city, cancellation))
+    //            .WithMessage("The city name must be in the exact format provided by the New Post.");
+    //    RuleFor(pud => pud.DeliveryAddress)
+    //        .MustAsync(async (customer, address, cancellation) => address == null
+    //            || customer.City != null && await newPostService.CheckIfAddressIsValidAsync(customer.City, address, cancellation))
+    //            .WithMessage("There is no New Post office at the specified address in the city.");
+    //}
 }
