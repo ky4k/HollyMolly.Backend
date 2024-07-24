c72374b3b6476f0899691a044399805e20b9ac32
Ky4k <6965090@stud.nau.edu.ua>
2024-07-24 15:21:22 +0300

    Modificated model of NewPostResponse

--- BLL/Models/NewPost/NewPostResponse.cs
+++ BLL/Models/NewPost/NewPostResponse.cs
@@ -9,7 +9,14 @@ namespace HM.BLL.Models.NewPost
     public class NewPostResponse<T> where T : class
     {
         public bool Success { get; set; }
-        public List<NewPostWarehouse> Data { get; set; } = null!;
+        public List<T> Data { get; set; } = null!;
         public List<string> Errors { get; set; } = null!;
+        public List<string> TranslatedErrors { get; set; } = null!;
+        public List<string> Warnings { get; set; } = null!;
+        public List<string> Info { get; set; } = null!;
+        public List<string> MessageCodes { get; set; } = null!;
+        public List<string> ErrorCodes { get; set; } = null!;
+        public List<string> WarningCodes { get; set; } = null!;
+        public List<string> InfoCodes { get; set; } = null!;
     }
 }
